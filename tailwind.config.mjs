0547a2f90b467dfeab8f33313e373e843b0cb7fd
Brad Johnson <brad@bradjohnson.io>
2024-11-04 10:21:39 -0500

    Tweaks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,20 +2,47 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ['class'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: ['Archivo', ...defaultTheme.fontFamily.sans]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ['Archivo', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        main: '#FFDC58',
+        mainAccent: '#ffc800', // not needed for shadcn components
+        overlay: 'rgba(0,0,0,0.8)', // background color overlay for alert dialogs, modals, etc.
+
+        // light mode
+        bg: '#FEF2E8',
+        text: '#000',
+        border: '#000',
+
+        // dark mode
+        darkBg: '#374151',
+        darkText: '#eeefe9',
+        darkBorder: '#000',
+        secondaryBlack: '#212121',
+      },
+      borderRadius: {
+        base: '5px',
+      },
+      boxShadow: {
+        light: '4px 4px 0px 0px #000',
+        dark: '4px 4px 0px 0px #000',
+      },
+      translate: {
+        boxShadowX: '4px',
+        boxShadowY: '4px',
+        reverseBoxShadowX: '-4px',
+        reverseBoxShadowY: '-4px',
+      },
+      fontWeight: {
+        base: '500',
+        heading: '700',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
