0cf7250b17d3c0d7d83825e1fae5c75e440ecd56
Brad Johnson <brad@bradjohnson.io>
2024-11-06 15:01:04 -0500

    Added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }), 
-    react()]
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), playformCompress()]
 });
